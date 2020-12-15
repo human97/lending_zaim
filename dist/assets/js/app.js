27864e7457f675191cd162107c27d9257db4479d
human97 <aleksey.n.popov@gmail.com>
2020-12-15 15:32:00 +0400

    add @media 800px

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -2741,7 +2741,7 @@ $(function () {
   $("#slick-slider").slick({
     arrows: false,
     dots: false,
-    autoplay: true,
+    //autoplay: true,
     speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1
